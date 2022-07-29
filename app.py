e9696b54465ac5707ab3854021d8a9e82a0a50ca
Arina_Nichko <oryna_nichko@epam.com>
2022-07-29 17:02:37 +0300

    First Commit

--- app.py
+++ app.py
@@ -7,20 +7,16 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
-
-BOT_TOKEN = '5444500594:AAE92u7a7mT2fWDFhSXRA8qs7eDpVP0ovZM'
-print(BOT_TOKEN)
+TOKEN = os.getenv('BOT_TOKEN')
+print(TOKEN)
 service_bot = Bot(token='5493235478:AAHGDhBrc1JZE0S3fQoy0Vfpmz3np6Ejoa0')
 print(service_bot)
 client_bot = Bot(token='5444500594:AAE92u7a7mT2fWDFhSXRA8qs7eDpVP0ovZM')
 dp = Dispatcher(client_bot)
 dp.middleware.setup(LoggingMiddleware())
-
-
-
-# webhook settings
-WEBHOOK_HOST = 'https://bot-for-work-kyiv.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 WEBAPP_HOST = '0.0.0.0'
 DEFAULT_PORT = 5000
@@ -28,7 +24,7 @@ DEFAULT_PORT = 5000
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
-if not BOT_TOKEN:
+if not TOKEN:
     print('You have forgot to set BOT_TOKEN')
     quit()
 
